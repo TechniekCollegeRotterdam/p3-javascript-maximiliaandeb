c545ed1eaaf31c7fd49f7b36112b4aa07d11ecb3
maximiliaandeb <9018883@student.zadkine.nl>
2023-02-02 11:17:10 +0100

    opdracht 2.2

---
+++ opdracht-2.2/script.js
@@ -0,0 +1,9 @@
+let text = document.getElementById("change");
+console.log(text);
+
+function kleur_aanpassen(){
+    text.style.color="red";
+    text.innerText="ik ben veranderd";
+    text.style.fontWeight = "bold"; 
+}
+

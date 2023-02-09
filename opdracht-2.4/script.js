9a3df467853a630c7e5d610dd4062484596cfdc4
maximiliaandeb <9018883@student.zadkine.nl>
2023-02-09 13:23:28 +0100

    opdracht 2.4

---
+++ opdracht-2.4/script.js
@@ -0,0 +1,23 @@
+// opdracht 1
+function calc(num, num2){
+    let answer = num + num2;
+    console.log(answer);
+    }
+    
+    calc(7, 8);
+
+// opdracht 2
+function multiplyByFive(num){
+    let answer = num * 5;
+    console.log(answer)
+}
+
+multiplyByFive(85);
+
+// opdracht 3
+function calculateMinuts(num){
+    let answer = num / 60
+    console.log(answer)
+}
+
+calculateMinuts(3600)
